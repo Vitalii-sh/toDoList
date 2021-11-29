058aec85faba69c7ae527ccc2fdf5b7b6832c36a
Vitalii-sh <Vitalik0505350@gmail.com>
2021-11-29 06:04:22 +0200

    add constants

--- src/app.js
+++ src/app.js
@@ -1,18 +1,9 @@
-const task = document.querySelector(".input");
-const toDoList = document.querySelector(".list");
-const addBtn = document.querySelector(".addBtn");
-const toDo = document.querySelector("#toDo");
-const label = toDo.content.querySelector("label");
-const delBtn = toDo.content.querySelector(".delBtn");
-const input = toDo.content.querySelector("input");
-let id = 1;
-let toDos;
-
 function toLocal() {
   toDos = toDoList.innerHTML;
   localStorage.setItem("toDos", toDos);
 }
-addBtn.addEventListener("click" && "keypres", () => {
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault;
   createTask();
   id++;
   toLocal();
